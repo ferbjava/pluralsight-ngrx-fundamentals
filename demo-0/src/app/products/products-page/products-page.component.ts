42d1e217c74f3c5a56bbba23dec2643b6d4643cc
Michal Kotecki <michal.kotecki@capgemini.com>
2026-05-18 08:52:01 +0200

    mod_10 completed

--- demo-0/src/app/products/products-page/products-page.component.ts
+++ demo-0/src/app/products/products-page/products-page.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { ProductsPageActions } from '../state/products.actions';
 import { selectProducts, selectProductsErrorMessage, selectProductsLoading, selectProductsTotal, selectShowProductsCode } from '../state/products.selectors';
+import { ProductsStore } from '../products.store';
 
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.css'],
+  providers: [ProductsStore],
 })
 export class ProductsPageComponent {
-  products$ = this.store.select(selectProducts);
+  // products$ = this.store.select(selectProducts);
+  products$ = this.productsStore.products$;
   total$ = this.store.select(selectProductsTotal);
   loading$ = this.store.select(selectProductsLoading);
   showProductCode$ = this.store.select(selectShowProductsCode);
@@ -17,10 +20,15 @@ export class ProductsPageComponent {
 
   constructor(
     private store: Store,
+    private productsStore: ProductsStore,
   ) {
     this.store.subscribe((state) => {
       console.log('State changed:', state);
     });
+  }
+
+  ngOnInit() {
+    this.productsStore.getProducts();
   }
 
   toggleShowProductCode() {
